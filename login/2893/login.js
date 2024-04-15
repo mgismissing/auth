801cf392766098d75ecad9506328587a67c14eda
mgismissing <gabrycraft2010@gmail.com>
2024-04-15 22:57:59 +0200

    Add login

--- login/2893/login.js
+++ login/2893/login.js
@@ -1,3 +1,3 @@
 fetch("id.txt")
   .then(response => response.text())
-  .then(text => document.getElementById("uuid4set").value = text.split("\n")[0]);
+  .then(text => document.getElementById("uuid4set").value = text.split("\n")[1]);
